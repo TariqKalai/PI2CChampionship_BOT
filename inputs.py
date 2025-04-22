801d7eaf28210819d15ab374c0ca1ccb0adec03d
TariqKalai <23047@ecam.be>
2025-04-22 11:49:49 +0200

    Update : Inputs ignore the order of letters

--- inputs.py
+++ inputs.py
@@ -4,20 +4,30 @@ def random_moves(request:dict) -> dict :
     '''Inputs random move based on the state off the game, does not make BAD MOVES'''
 
     pos = None
-    pieces = ["BDEC", "BDEP", "BDFC", "BDFP", "BLEC", "BLEP", 
+    set_pieces = []
+    List_pieces = ["BDEC", "BDEP", "BDFC", "BDFP", "BLEC", "BLEP", 
               "BLFC", "BLFP", "SDEC", "SDEP", "SDFC", "SDFP", 
                 "SLEC", "SLEP", "SLFC", "SLFP"]
+    
+    for i in List_pieces:
+        set_pieces.append(set(i))
+
     
     #removes pieces from the list[pieces] based on the state. Removes all the pieces who are already on the board
     for i in request["state"]["board"] :
         if i != None :
-            for x in pieces:
-                if all(elements in x for elements in i[:4]):
-                    pieces.remove(x)
+            for x in set_pieces:
+                if x == set(i):
+                    set_pieces.remove(set(x))
 
-    #removes the pieces the player will be playing in this round.
-    if request["state"]["piece"] in pieces:
-        pieces.remove(request["state"]["piece"])
+    
+    #removes the pieces the player will be playing in this round. if the player starts and request["state"]["piece"] == None 
+    #it make a TypeError so we pass
+    try:
+        if set(request["state"]["piece"]) in set_pieces:
+            set_pieces.remove(set(request["state"]["piece"]))
+    except TypeError:
+        pass
     
     #makes a Random move
     while True:
@@ -29,11 +39,16 @@ def random_moves(request:dict) -> dict :
 
     #checks if pieces are still availabe for the opponent
     
-    if len(pieces) == 0 :
+    if len(set_pieces) == 0 :
         oppenent_piece = None
 
     else:
-        oppenent_piece = pieces[randint(0,len(pieces)-1)]
+        oppenent_piece_set = set_pieces[randint(0,len(set_pieces)-1)]
+        oppenent_piece = ""
+
+        for i in oppenent_piece_set :
+            oppenent_piece += i
+
 
     
 
@@ -44,3 +59,4 @@ def random_moves(request:dict) -> dict :
     }
 
     return response
+
