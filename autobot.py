2b8684723044159e004a6a3c13b343199f40cfdf
TariqKalai <23047@ecam.be>
2025-04-23 11:16:22 +0200

    simplify inputs : check available pieces

--- autobot.py
+++ autobot.py
@@ -3,9 +3,9 @@ from threading import Thread
 from time import sleep
 
 # Run another script and provide input
-port = [123,725,1928,5263,1671,8735,1631]
-player=["Powerpoint Ranger", "Starbucks wars", "Harry Plotter",'Autobotter', 'Bytes10', 'The green array']
-matricules=["456","2341","1565","2563","1383","28731"]
+port = [123,725,1928,5263,1671,8735,1631,6426]
+player=["Powerpoint Ranger", "Starbucks wars", "Harry Plotter",'Autobotter', 'Bytes10', 'The green array','La vache qui print']
+matricules=["456","2341","1565","2563","1383","28731", "17876"]
 
 def launch(i):
     '''Launches the client program in a Local server to enable the use of multiple bots for testing and debugging purposes.'''
@@ -17,13 +17,13 @@ def launch(i):
     )
 
 #Launches the program 4 times
-for i in range(6):
+for i in range(7):
 
-    print([str(port[i]), player[i], "LOCALHOST", "3000", str(matricules[i])])
+    print("Start :" ,[str(port[i]), player[i], "LOCALHOST", "3000", str(matricules[i])])
 
     thread = Thread(target=launch, args=(i,))
     thread.start()
-    sleep(2)
+    sleep(0.5)
     
     
 
